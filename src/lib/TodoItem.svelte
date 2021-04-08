8e52209f5849512cf748e6a5f1477f3bb991a75b
Jake Runzer <jakerunzer@gmail.com>
2021-04-08 00:33:22 -0700

    init commit

---
+++ src/lib/TodoItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { ITodoItem } from "../types";
+  import { createEventDispatcher } from "svelte";
+
+  export let item: ITodoItem;
+
+  const dispatch = createEventDispatcher();
+
+  const onCheck = () => dispatch("check", { completed: !item.completed });
+</script>
+
+<li>
+  <label>
+    <input type="checkbox" checked={item.completed} on:change={onCheck} />
+    <span class="pl-2 text-lg">{item.text}</span>
+  </label>
+</li>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import TodoList from "$lib/TodoList.svelte";
+</script>
+
+<main class="max-w-sm mx-auto">
+  <header class="mt-12 mb-8">
+    <h1 class="text-6xl text-blue-500 font-bold text-center mb-4">Todo</h1>
+    <h2 class="text-gray-500 text-center">
+      A simple todo list created with SvelteKit + Tailwind
+    </h2>
+  </header>
+
+  <TodoList />
+</main>
